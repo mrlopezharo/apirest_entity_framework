1343d5109c21c2d32357e2398d20848db7e62206
mrlopezharo <lopezharomr@gmail.com>
2022-05-19 08:58:43 -0600

    Agregar archivos de proyecto.

---
+++ apirest_entity_framework/Data/apirest_entity_frameworkContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using apirest_entity_framework.Models;
+
+namespace apirest_entity_framework.Data
+{
+    public class apirest_entity_frameworkContext : DbContext
+    {
+        public apirest_entity_frameworkContext (DbContextOptions<apirest_entity_frameworkContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<apirest_entity_framework.Models.Book> Book { get; set; }
+    }
+}
